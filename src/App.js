e89bfac961f43bc3a15b843beddab06a7a404e76
Mark Mbithi <markmbila@gmail.com>
2021-05-15 17:55:08 +0300

    Example Counter with Redux
    We are doing a simple counter here with actions. We are storing the value of counter in a global store.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
 }
+const store = createStore(reducer);
+//import App from './App';
+
+const App = () => (
+  <Provider store={store}>
+    <Counter/>
+  </Provider>
+);
 
 export default App;
